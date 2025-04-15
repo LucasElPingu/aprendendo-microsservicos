9f7e83d0e1a73a1285fd1ad825dbdf69c9d10297
LucasElPingu <lucasfernando.soaresssilva@gmail.com>
2025-04-15 18:58:33 -0300

    Criando a classe payroll

---
+++ hr-payroll/src/test/java/com/lucaselpingu/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaselpingu.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
