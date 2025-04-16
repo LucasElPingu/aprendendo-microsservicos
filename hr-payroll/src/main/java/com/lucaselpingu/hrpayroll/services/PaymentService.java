169580c060c30b175003c385689e539c42970dec
LucasElPingu <lucasfernando.soaresssilva@gmail.com>
2025-04-15 21:12:45 -0300

    Implementando feignClient

--- hr-payroll/src/main/java/com/lucaselpingu/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/lucaselpingu/hrpayroll/services/PaymentService.java
@@ -1,34 +1,20 @@
 package com.lucaselpingu.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.lucaselpingu.hrpayroll.entites.Payment;
 import com.lucaselpingu.hrpayroll.entites.Worker;
+import com.lucaselpingu.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 
 	@Autowired
-	private RestTemplate restTemplate;
-	//permite atribuir o valor da propiedade a uma variavel 
-	@Value("${hr-worker.host}")
-	private String workerHost;
+	private WorkerFeignClient workerFeign;
 	
 	public Payment getPayment(long id, int days) {
-		//vai precisar chamar no microsservico workers o worker pelo id
-		//faz a chamada no webservice no workers
-		//faz a requisição a uma API externa usando resttemplate
-		//como passar parametros em uma requisição resttemplate usando o getforobject, tem que criar um mapa
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", String.valueOf(id));
-		//url começa com o host + o caminho e paramentro entre chaves, tipo do objeto, o parametros
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		Worker worker = workerFeign.findById(id).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
